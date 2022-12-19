0a90f6beca4f002de6d8cf97ae8636e3aecb7b25
Mourad <mourad.elhadj.mimoune@akretion.com>
2022-12-19 16:40:21 +0100

    [FIX][14.0] helpdesk_mgmt_stock: split piking in & picking out

--- helpdesk_mgmt_stock/models/helpdesk_ticket.py
+++ helpdesk_mgmt_stock/models/helpdesk_ticket.py
@@ -13,27 +13,66 @@ class HelpdeskTicket(models.Model):
         inverse_name="helpdesk_ticket_id",
         help="Linked stock picking to the current Ticket",
     )
-
-    stock_picking_count = fields.Integer(
-        "Stock Picking number", compute="_compute_stock_picking_count"
+    stock_picking_out_ids = fields.One2many(
+        string="Stock Picking out",
+        comodel_name="stock.picking",
+        compute="_compute_stock_picking_count",
+        help="Linked Stock Picking out to the current Ticket",
+    )
+    stock_picking_in_ids = fields.One2many(
+        string="Stock Picking in",
+        comodel_name="stock.picking",
+        compute="_compute_stock_picking_count",
+        help="Linked Stock Picking in to the current Ticket",
+        domain="[('picking_type', 'in', ('in_invoice','in_refund'))]",
+    )
+    stock_picking_out_count = fields.Integer(
+        "Stock Picking out number", compute="_compute_stock_picking_count"
+    )
+    stock_picking_in_count = fields.Integer(
+        "Stock Picking in number", compute="_compute_stock_picking_count"
     )
 
-    @api.depends("stock_picking_ids", )
+    @api.depends(
+        "stock_picking_ids",
+    )
     def _compute_stock_picking_count(self):
         for rec in self:
-            rec.stock_picking_count = len(rec.stock_picking_ids)
+            rec.stock_picking_out_ids = rec.stock_picking_ids.filtered(
+                lambda l: l.picking_type_id.code == "outgoing"
+            )
+            rec.stock_picking_in_ids = rec.stock_picking_ids.filtered(
+                lambda l: l.picking_type_id.code == "incomming"
+            )
+            rec.stock_picking_out_count = len(rec.stock_picking_out_ids)
+            rec.stock_picking_in_count = len(rec.stock_picking_in_ids)
 
-    def action_view_stock_picking(self):
+    def action_view_stock_picking_out(self):
         action = self.env["ir.actions.actions"]._for_xml_id(
             "stock.action_picking_tree_all"
         )
-        action["domain"] = [("id", "in", self.stock_picking_ids.ids),
-                            ('picking_type', 'in', ('out_invoice', 'out_refund'))]
+        action["domain"] = [
+            ("id", "in", self.stock_picking_out_ids.ids),
+        ]
         action["context"] = {
             "default_helpdesk_ticket_id": self.id,
-            'contact_display': 'partner_address',
+            "contact_display": "partner_address",
         }
-        if len(self.stock_picking_ids) == 1:
+        if len(self.stock_picking_out_ids) == 1:
             action["views"] = [(self.env.ref("stock.view_picking_form").id, "form")]
-            action["res_id"] = self.stock_picking_ids.id
+            action["res_id"] = self.stock_picking_out_ids.id
+        return action
+
+    def action_view_stock_picking_in(self):
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "stock.action_picking_tree_all"
+        )
+        action["domain"] = [("id", "in", self.stock_picking_in_ids.ids)]
+        action["context"] = {
+            "default_helpdesk_ticket_id": self.id,
+            "contact_display": "partner_address",
+        }
+        if len(self.stock_picking_in_ids) == 1:
+            action["views"] = [(self.env.ref("stock.view_picking_form").id, "form")]
+            action["res_id"] = self.stock_picking_in_ids.id
         return action
