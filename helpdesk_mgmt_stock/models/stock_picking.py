3a204eca7c3d5c96d4ef35aa9bb41e9f12b71fd1
Mourad <mourad.elhadj.mimoune@akretion.com>
2023-01-24 16:10:09 +0100

    [IMP][14.0] helpdesk_mgmt_stock: add groups on helpdesk_ticket_id & imp view

--- helpdesk_mgmt_stock/models/stock_picking.py
+++ helpdesk_mgmt_stock/models/stock_picking.py
@@ -8,5 +8,7 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     helpdesk_ticket_id = fields.Many2one(
-        string="Tickets", comodel_name="helpdesk.ticket"
+        string="Tickets",
+        comodel_name="helpdesk.ticket",
+        groups="helpdesk_mgmt.group_helpdesk_user_own",
     )
